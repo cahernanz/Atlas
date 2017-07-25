ef95e3f309199150cc9fc040c8e0911e3e0d4ca6
gherardovarando <gherardo.varando@gmail.com>
2017-07-25 12:29:46 +0200

    migrate to MIT license

--- app/main.js
+++ app/main.js
@@ -1,3 +1,23 @@
+// Copyright (c) 2016 Gherardo Varando
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 const {
     fork,
     exec
@@ -91,13 +111,6 @@ ipcMain.on("focusWindow", () => {
 
 ipcMain.on("openDevTools", () => {
     win.webContents.openDevTools()
-})
-
-ipcMain.on("imageJ", (event, arg) => {
-    let path = app.getPath('exe');
-    exec('java -Xmx512m -jar ij.jar', {
-        cwd: `${process.resourcesPath}/ImageJ/`
-    }, (error, stdout, stderr) => {})
 })
 
 ipcMain.on('setProgress', (event, arg) => {
