d31130cf158f886467a22a4b6aa6f416a33c36e2
gherardovarando <gherardo.varando@gmail.com>
2017-09-29 09:37:19 +0200

    update

--- app/mainWindow.js
+++ app/mainWindow.js
@@ -19,14 +19,14 @@ let t = new Date();
 
 const isDev = require('electron-is-dev');
 const {
-    gui,
-    Workspace,
-    ExtensionsManager,
-    TasksViewer
+  gui,
+  Workspace,
+  ExtensionsManager,
+  TasksViewer
 } = require(`electrongui`);
 const {
-    Menu,
-    MenuItem
+  Menu,
+  MenuItem
 } = require('electron').remote;
 const HelpExtension = require('helpextension');
 const MapExtension = require('mapextension');
@@ -36,65 +36,65 @@ const Shiny = require('rshinyextension');
 
 //prevent app closing
 document.addEventListener('dragover', function(event) {
-    event.preventDefault();
-    return false;
+  event.preventDefault();
+  return false;
 }, false);
 
 document.addEventListener('drop', function(event) {
-    event.preventDefault();
-    return false;
+  event.preventDefault();
+  return false;
 }, false);
 gui.startWaiting();
 if (isDev) {
-    gui.addMenuItem(new MenuItem({
-        label: "Dev",
-        type: 'submenu',
-        submenu: Menu.buildFromTemplate([{
-            label: 'toggledevtools',
-            role: "toggledevtools"
-        }])
-    }));
+  gui.addMenuItem(new MenuItem({
+    label: "Dev",
+    type: 'submenu',
+    submenu: Menu.buildFromTemplate([{
+      label: 'toggledevtools',
+      role: "toggledevtools"
+    }])
+  }));
 }
 gui.addMenuItem(new MenuItem({
-    label: "File",
-    type: "submenu",
-    submenu: Menu.buildFromTemplate([{
-            label: 'New Workspace',
-            click: () => {
-                if (gui.workspace instanceof Workspace) {
-                    gui.workspace.newChecking();
-                }
-            }
-        },
-        {
-            label: 'Open Workspace',
-            click: () => {
-                if (gui.workspace instanceof Workspace) {
-                    gui.workspace.loadChecking();
-                }
-            }
-        },
-        {
-            label: 'Save Workspace',
-            click: () => {
-                if (gui.workspace instanceof Workspace) {
-                    gui.workspace.save(gui.workspace.spaces.workspace.path);
-                }
-            }
-        },
-        {
-            label: 'Save Workspace as',
-            click: () => {
-                if (gui.workspace instanceof Workspace) {
-                    gui.workspace.save();
-                }
-            }
-        },
-        {
-            label: 'Quit',
-            role: 'quit'
+  label: "File",
+  type: "submenu",
+  submenu: Menu.buildFromTemplate([{
+      label: 'New Workspace',
+      click: () => {
+        if (gui.workspace instanceof Workspace) {
+          gui.workspace.newChecking();
         }
-    ])
+      }
+    },
+    {
+      label: 'Open Workspace',
+      click: () => {
+        if (gui.workspace instanceof Workspace) {
+          gui.workspace.loadChecking();
+        }
+      }
+    },
+    {
+      label: 'Save Workspace',
+      click: () => {
+        if (gui.workspace instanceof Workspace) {
+          gui.workspace.save(gui.workspace.spaces.workspace.path);
+        }
+      }
+    },
+    {
+      label: 'Save Workspace as',
+      click: () => {
+        if (gui.workspace instanceof Workspace) {
+          gui.workspace.save();
+        }
+      }
+    },
+    {
+      label: 'Quit',
+      role: 'quit'
+    }
+  ])
 }));
 
 gui.extensionsManager = new ExtensionsManager();
@@ -102,13 +102,14 @@ gui.workspace = new Workspace();
 gui.helpExtension = new HelpExtension();
 gui.tasksViewer = new TasksViewer();
 gui.mapExtension = new MapExtension();
+new ImageJ();
 new Shiny();
 new GM();
 gui.helpExtension.activate();
 gui.tasksViewer.activate();
 gui.mapExtension.activate();
+gui.extensions.ImageJExtension.activate();
 gui.mapExtension.show();
-new ImageJ().activate();
 gui.stopWaiting();
 gui.viewTrick();
 gui.notify(`App loaded in ${(new Date())-t} ms`);
